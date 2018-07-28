36b5156b518c96cba95e86da1c6f431528e9aeb2
Matthew Eddy <meddy672@gmail.com>
2018-07-28 18:17:49 -0400

    setup location.dart to handle map functions

---
+++ lib/widgets/form_inputs/location.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:map_view/map_view.dart';
+import '../helpers/ensure_visible.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LocationInput();
+  }
+}
+
+class _LocationInput extends State<LocationInput> {
+  final FocusNode _addressInputFocusNode = FocusNode();
+
+  @override
+    void initState() {
+      _addressInputFocusNode.addListener(_updateLocation);
+      super.initState();
+    }
+
+  @override
+  void dispose(){
+    _addressInputFocusNode.removeListener(_updateLocation);
+    super.dispose();
+  }
+  void _updateLocation(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        EnsureVisibleWhenFocused(
+          focusNode: _addressInputFocusNode,
+          child: TextFormField(
+            focusNode: _addressInputFocusNode,
+          ),
+        )
+      ],
+    );
+  }
+}
